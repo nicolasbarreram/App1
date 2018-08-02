cc8f86f23b6601cc0d321f72b93d5ec5fb56fdbd
Nicolas <nikolasmedina68@gmail.com>
2018-08-02 13:39:41 -0500

    Initial commit

---
+++ App1/App1/Views.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Views : ContentPage
+    {
+        public Views()
+        {
+            InitializeComponent();
+            dtpFecha.MinimumDate = new DateTime(1950, 2, 15);
+            dtpFecha.DateSelected += DtpFecha_DateSelected;
+            edComentario.Completed += EdComentario_Completed;
+
+        }
+
+        private void EdComentario_Completed(object sender, EventArgs e)
+        {
+            DisplayAlert("Finalizado", "Terminado de editar", "Listo");
+        }
+
+        private void DtpFecha_DateSelected(object sender, DateChangedEventArgs e)
+        {
+
+            DisplayAlert("Fecha", e.NewDate.ToString(), "aceptar");
+        }
+
+        private void BtnEnviarClicked(object sender, EventArgs args)
+        {
+            DisplayAlert("Ingreso", "Bienvenido", "Aceptar");
+        }
+
+    }
+}
